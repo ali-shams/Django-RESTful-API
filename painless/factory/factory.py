d931d15879263234d6436f11ced55d6d50ccfa76
ali-shams <ali.shamsadn@gmail.com>
2023-01-13 18:03:51 +0330

    feat(account): add factory layer

---
+++ painless/factory/factory.py
@@ -0,0 +1,121 @@
+import re
+from abc import ABC, abstractmethod
+
+from ..helper.enums import RegexOperatorPattern
+
+
+def getOTP(phone_number):
+    for operator in RegexOperatorPattern:
+        if re.compile(operator.value).match(phone_number):
+            operator = operator.name
+            break
+    formats = {
+        "HamraheAval": HamraheAvalPhoneNumber,
+        "Irancell": IrancellPhoneNumber,
+        "Rightel": RightelPhoneNumber,
+        "ShatelMobile": ShatelMobilePhoneNumber,
+        "Samantel": SamantelPhoneNumber,
+        "Taliya": TaliyaPhoneNumber,
+        "Other": OtherPhoneNumber,
+    }
+    result = formats[operator](phone_number)
+    return result.call_send_otp()
+
+
+# ############################### #
+#            Creator              #
+# ############################### #
+class Creator(ABC):
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
+
+    @abstractmethod
+    def factory_method(self):
+        ...
+
+    def call_send_otp(self):
+        product = self.factory_method()
+        result = product.send_otp(self.phone_number)
+        return result
+
+
+# ############################### #
+#        ConcreteCreator          #
+# ############################### #
+class HamraheAvalPhoneNumber(Creator):
+    def factory_method(self):
+        return HamraheAval()
+
+
+class IrancellPhoneNumber(Creator):
+    def factory_method(self):
+        return Irancell()
+
+
+class RightelPhoneNumber(Creator):
+    def factory_method(self):
+        return Rightel()
+
+
+class ShatelMobilePhoneNumber(Creator):
+    def factory_method(self):
+        return ShatelMobile()
+
+
+class SamantelPhoneNumber(Creator):
+    def factory_method(self):
+        return Samantel()
+
+
+class TaliyaPhoneNumber(Creator):
+    def factory_method(self):
+        return Taliya()
+
+
+class OtherPhoneNumber(Creator):
+    def factory_method(self):
+        return Other()
+
+
+# ############################### #
+#             Product             #
+# ############################### #
+class Product(ABC):
+    @abstractmethod
+    def send_otp(self):
+        ...
+
+
+class HamraheAval(Product):
+    def send_otp(self, phone_number):
+        return f"HamraheAval_{phone_number}"
+
+
+class Irancell(Product):
+    def send_otp(self, phone_number):
+        return f"Irancell_{phone_number}"
+
+
+class Rightel(Product):
+    def send_otp(self, phone_number):
+        return f"Rightel_{phone_number}"
+
+
+class ShatelMobile(Product):
+    def send_otp(self, phone_number):
+        return f"ShatelMobile_{phone_number}"
+
+
+class Samantel(Product):
+    def send_otp(self, phone_number):
+        return f"Samantel_{phone_number}"
+
+
+class Taliya(Product):
+    def send_otp(self, phone_number):
+        return f"Taliya_{phone_number}"
+
+
+class Other(Product):
+    def send_otp(self, phone_number):
+        return f"Other_{phone_number}"
--- painless/helper/enums.py
+++ painless/helper/enums.py
@@ -2,5 +2,20 @@ from enum import Enum
 
 
 class RegexPatternEnum(Enum):
-    Iran_phone_number = r"^(\+98|0)?9\d{9}$"
+    Iran_phone_number = r"(\+989\d{9})|(09\d{9})$"
     International_phone_number = r"^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$"
+
+
+class RegexOperatorPattern(Enum):
+    # HamraheAval: 091[0-9] | 099[0-6]
+    HamraheAval = r"(\+9891[0-9]\d{7})|(091[0-9]\d{7})$"
+    # Irancell: 090[0-5] | 093[0]|[3]|[5-9]
+    Irancell = r"(\+9890[0-5]\d{7})|(090[0-5]\d{7})|(\+9893([0]|[3]|[5-9])\d{7})|(093([0]|[3]|[5-9])\d{7})$"
+    # Rightel: 092[0-3]
+    Rightel = r"(\+9892[0-3]\d{7})|(092[0-3]\d{7})$"
+    # ShatelMobile: 0998
+    ShatelMobile = r"(\+98998\d{7})|(0998\d{7})$"
+    # Samantel: 0999
+    Samantel = r"(\+98999\d{7})|(0999\d{7})$"
+    # Taliya: 0932
+    Taliya = "(\+98932\d{7})|(0932\d{7})$"
